8748836209a051562185af911a8971400bf9eeb5
om-daudia <daudiaom@gmail.com>
2026-07-18 15:57:10 +0530

    project initialize

---
+++ src/main/java/com/odschool/OdschoolApplication.java
@@ -0,0 +1,13 @@
+package com.odschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odschool/OdschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
